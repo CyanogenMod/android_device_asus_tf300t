4b4e35df6d45428777a99a606a0612e66c70cfab
Wouter Heijligen <wouter2702@hotmail.com>
2012-11-13 09:11:18 +0100

    Add LCD_Density = 160
    This will fix issues with Untermensch Overclock kernel and keyboard etc.

--- cm.mk
+++ cm.mk
@@ -12,6 +12,7 @@ PRODUCT_BRAND := asus
 PRODUCT_DEVICE := tf300t
 PRODUCT_MODEL := Transformer Pad TF300T
 PRODUCT_MANUFACTURER := asus
+RO.SF.LCD_DENSITY := 160
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/WW_epad/EeePad:4.0.3/IML74K/WW_epad-9.4.3.29-20120511:user/release-keys PRIVATE_BUILD_DESC="WW_epad-user 4.0.3 IML74K WW_epad-9.4.3.29-20120511 release-keys"
 
 # Release name and versioning
